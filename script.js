0bab7409a30915e12bc0e87315e1cd5f6289af36
ajmalpk <ajmalpk016@gmail.com>
2024-08-26 08:56:01 +0530

    html page routing handele

--- script.js
+++ script.js
@@ -40,53 +40,107 @@
 
 
 
+
+//   this is / shlash page
+
+
+// fs.mkdir(path.join(__dirname,"/view"),{},(err)=>{
+   
+//     if(err) throw err;
+//     console.log("folder created")
+// })
+
+// fs.writeFile(path.join(__dirname,"view","index.html"),"/  : this is my page hash page",{},(err)=>{
+//     if(err) throw err;
+
+// })
+
+
+// this this is my aboutepage createCode
+
+
+// fs.mkdir(path.join(__dirname,"about"),{},(err)=>{
+//     if(err) throw err;
+//     console.log("folder created")
+
+// })
+
+
+// fs.writeFile(path.join(__dirname,"about","about.html"),"welcome my about page",{},(err)=>{
+
+//     if(err) throw err;
+//     console.log('succussFully writed');
+// })
+
+
+
+// create conatct folder valus addedd
+
+
+// fs.mkdir(path.join(__dirname,"conatct"),{},(err)=>{
+//     if(err) throw err;
+//     console.log("contact folder created")
+// })
+
+
+// fs.writeFile(path.join(__dirname,"/conatct","index.html"),"welcome my contact page",{},(err)=>{
+//     if(err) throw err;
+//     console.log("values added contact")
+// })
+
+
 const http= require("http")
 const path= require("path")
 const fs =require("fs")
 
-  this is / shlash page
 
 
-fs.mkdir(path.join(__dirname,"/view"),{},(err)=>{
-   
-    if(err) throw err;
-    console.log("folder created")
+const server = http.createServer((req,res)=>{
+
+    if(req.url==="/view"){
+
+    fs.readFile(path.join(__dirname,"/view","index.html"),"utf8",(err,data)=>{
+        if(err) throw err;
+        
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.end(data)
+
+    })
+    }
+
+
+    if(req.url==="/about"){
+
+        fs.readFile(path.join(__dirname,"/about","about.html"),"utf8",(err,data)=>{
+             
+               if(err) throw err;
+
+               res.writeHead(200,{'Content-type':'text/html'})
+               res.end(data)
+        })
+    }
+
+
+    if(req.url==="/conatct"){
+        fs.readFile(path.join(__dirname,"/conatct","index.html"),"utf8",(err,data)=>{
+
+            if(err)  throw err;
+
+            res.writeHead(200,{'Content-type':'text/html'})
+            res.end(data)
+
+        })
+    }
+  
+
+
 })
 
-fs.writeFile(path.join(__dirname,"view","index.html"),"/  : this is my page hash page",{},(err)=>{
-    if(err) throw err;
 
+const port= process.env.PORT||3001
+
+
+server.listen(port,()=>{
+    console.log("server is running");
 })
 
-
-this this is my aboutepage createCode
-
-
-fs.mkdir(path.join(__dirname,"about"),{},(err)=>{
-    if(err) throw err;
-    console.log("folder created")
-
-})
-
-
-fs.writeFile(path.join(__dirname,"about","about.html"),"welcome my about page",{},(err)=>{
-
-    if(err) throw err;
-    console.log('succussFully writed');
-})
-
-
-
-create conatct folder valus addedd
-
-
-fs.mkdir(path.join(__dirname,"conatct"),{},(err)=>{
-    if(err) throw err;
-    console.log("contact folder created")
-})
-
-
-fs.writeFile(path.join(__dirname,"/conatct","index.html"),"welcome my contact page",{},(err)=>{
-    if(err) throw err;
-    console.log("values added contact")
-})
